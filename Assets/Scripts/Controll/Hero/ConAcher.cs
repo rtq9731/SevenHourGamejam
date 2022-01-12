1cfc1ea3e8692ed407014804558d01ab637d256d
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-01-12 14:21:59 +0900

    스크립트 수정

--- Assets/Scripts/Controll/Hero/ConAcher.cs
+++ Assets/Scripts/Controll/Hero/ConAcher.cs
@@ -13,7 +13,11 @@ public class ConAcher : CONCharacter
     {
         this.speed = speed;
         this.damage = damage;
-        Attack(damage); // 원래 함수 실행
+        CONEntity nearObj = GameSceneClass.gMGPool.poolTotalDic[ePrefabs.Monster].Find(x => x.gameObject.activeSelf);
+        if (nearObj != null)
+        {
+            Attack(damage); // 원래 함수 실행
+        }
     }
 
     public void Throw()
